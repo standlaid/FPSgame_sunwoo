c3fe324a363c050a6ede3b41fefcd3a6d371c272
standlaid <standlaid@gmail.com>
2023-08-17 17:51:05 +0900

    feat: 무기(폭탄), 무기 발사, 무기 이펙트 추가
    무기(폭탄)
    -bomb (Prefab)
    무기 발사
    - PlayerFire.cs
    무기 이펙트 추가
    - 에셋 적용 EffectDestroy -> Loop off

---
+++ Assets/Scripts/EffectDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//목적: 나(이펙트)가 특정 시간이 지나면 제거된다.
+public class EffectDestroy : MonoBehaviour
+{
+    public float destroyTime = 0;
+    public float currentTime = 0;
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime > destroyTime)
+        {
+            Destroy(gameObject);
+            currentTime = 0;
+        }
+    }
+
+}
